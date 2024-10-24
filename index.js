5c7f800ea927b76b3ec2735ce9b7d23ff08da911
Nitin Gupta <nitinguptamyself@gmail.com>
2024-10-24 13:23:32 +0530

    added quiz path to get the quizzes

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
 
-const app = express(); //creating a server
+const quizRouter = require("./router/quiz.router");
+const categoriesRouter = require("./router/categories.router");
+const { loginRouter, signupRouter } = require("./router/auth.router");
+const routeNotFound = require("./middleware/routeNotFound");
+const quizzes = require("./db/quizzes");
+
+const app = express(); //Creating a server
+app.use(cors());
+app.use(express.json());
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello Geeks");
-})
+  res.send("hello geeks");
+});
+
+app.use("/categories", categoriesRouter);
+app.use("/quiz", quizRouter);
+app.use("/auth/login", loginRouter);
+app.use("/auth/signup", signupRouter);
+app.use(routeNotFound);
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log("server started......");
-    
-})
+  console.log("server started....");
+});
